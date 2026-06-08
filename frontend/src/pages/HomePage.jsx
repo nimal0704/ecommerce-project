14e3332a46aa5ace26198dd48e37affdfa713392
Nimal <nimalmsv@gmail.com>
2026-06-08 19:39:10 +0530

    use axios to fetch the data

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import './HomePage.css'
 import '../components/Header'
 import { Header } from '../components/Header';
@@ -5,6 +6,13 @@ import { products } from '../../starting-code/data/products'
 
 
 export function HomePage(){
+
+  axios.get('http://localhost:3000/api/products')
+    .then((response) => {
+      console.log(response.data);
+
+    })
+
   return(
     <>
      <title>Ecommerce Project</title>
